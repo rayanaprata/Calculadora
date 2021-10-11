1b03628a71abde9eb2651c8f70bca5d1131a6f0b
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-11 11:35:03 -0300

    implementando alguns cálculos

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: Properties
-    var numberOfScreen: Double = 0
+    var numberOnScreen: Double = 0
+    var previousNumber: Double = 0
+    var performingMath = false
+    var operation = 0
     
     // MARK: Outlets
     @IBOutlet weak var labelValue: UILabel!
@@ -26,17 +29,78 @@ class ViewController: UIViewController {
     
     // MARK: Actions
     @IBAction func numbers(_ sender: Any) {
-        labelValue.text = labelValue.text! + String((sender as AnyObject).tag)
-        numberOfScreen = Double(labelValue.text!)!
+        
+        if performingMath {
+            labelValue.text = String((sender as AnyObject).tag)
+            
+            guard let labelVal = labelValue.text else { return }
+            numberOnScreen = Double(labelVal) ?? 0
+            performingMath = false
+        } else {
+            guard let label = labelValue.text else { return }
+            labelValue.text = label + String((sender as AnyObject).tag)
+            
+            guard let value = labelValue.text else { return }
+            
+            guard let doubleLabel = Double(value) else { return }
+            numberOnScreen = doubleLabel
+        }
     }
     
-    @IBAction func allCancel(_ sender: Any) {
-        labelValue.text = "0"
+    @IBAction func buttons(_ sender: UIButton) {
+        if labelValue.text != "" {
+            guard let labelVal = labelValue.text else { return }
+            previousNumber = Double(labelVal) ?? 0
+            
+            switch sender.tag {
+            case 11:
+                labelValue.text = ""
+                previousNumber = 0
+                numberOnScreen = 0
+                operation = 0
+            case 12:
+                print("divide")
+                labelValue.text = "÷"
+                operation = sender.tag
+                performingMath = true
+            case 13:
+                print("multiply")
+                labelValue.text = "×"
+                operation = sender.tag
+                performingMath = true
+            case 14:
+                print("minus")
+                labelValue.text = "-"
+                operation = sender.tag
+                performingMath = true
+            case 15:
+                print("plus")
+                labelValue.text = "+"
+                operation = sender.tag
+                performingMath = true
+            case 16:
+                switch operation {
+                case 12:
+                    labelValue.text = String(previousNumber / numberOnScreen)
+                case 13:
+                    labelValue.text = String(previousNumber * numberOnScreen)
+                case 14:
+                    labelValue.text = String(previousNumber - numberOnScreen)
+                case 15:
+                    labelValue.text = String(previousNumber + numberOnScreen)
+                default:
+                    break
+                }
+            default:
+                break
+            }
+        }
     }
+    
     
     // MARK: Methods
     private func setupUI() {
-        
+        labelValue.text = ""
     }
 }
 
